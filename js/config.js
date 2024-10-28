8eb0fe86a4afd98c421ed94af2c69cc16050c32a
Jcolecole <collinsoj78@gmail.com>
2024-10-28 16:19:42 -0400

    Update config.js
    updated config file.

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_AJPNDqYPZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5t72pi4vbf43mvm9pf8o6koufb ', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
